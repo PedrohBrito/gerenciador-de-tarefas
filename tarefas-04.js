9a11855e7bb56a25c80375763b455b7f564c4fce
Pedro <pedro.h.cunha16@aluno.senai.br>
2025-05-30 09:28:00 -0300

    feat: Remove primeira tarefa

---
+++ tarefas-04.js
@@ -0,0 +1,8 @@
+let tarefas = ["Estudar JavaScript", "Fazer exercícios de matemática", "Lavar a louça", 
+    "Comprar mercado", "Ir à academia", "Ler um livro", "Fazer revisão para o teste", 
+    "Limpar o quarto", "Estudar para concurso", "Organizar o computador"];
+
+// "Removendo primeira tarefa" 
+tarefas.shift();
+console.log(tarefas);
+  
