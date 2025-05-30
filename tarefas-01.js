acfe2e5bf11f54ac95376d50e1eec213e7a7d8f0
Pedro <pedro.h.cunha16@aluno.senai.br>
2025-05-30 09:03:16 -0300

    feat: Adiciona novas tarefas

---
+++ tarefas-01.js
@@ -0,0 +1,8 @@
+let tarefas = ["Estudar JavaScript", "Fazer exercícios de matemática", "Lavar a louça", 
+    "Comprar mercado", "Ir à academia", "Ler um livro", "Fazer revisão para o teste", 
+    "Limpar o quarto", "Estudar para concurso", "Organizar o computador"];
+
+// Adicionando 2 novas tarefas
+ tarefas.push("Lavar as roupas", "Estender as roupas");
+
+ 
