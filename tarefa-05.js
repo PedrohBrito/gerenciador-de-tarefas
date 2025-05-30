54440ba30712c4d6fed4811d59e92aef6f45abf0
Pedro <pedro.h.cunha16@aluno.senai.br>
2025-05-30 09:42:24 -0300

    "Verifica existência de tarefa"

---
+++ tarefa-05.js
@@ -0,0 +1,11 @@
+let tarefas = ["Estudar JavaScript", "Fazer exercícios de matemática", "Lavar a louça", 
+    "Comprar mercado", "Ir à academia", "Ler um livro", "Fazer revisão para o teste", 
+    "Limpar o quarto", "Estudar para concurso", "Organizar o computador"];
+
+// Verificação existência de tarefa
+
+    if (tarefas.includes("Ir à academia")) {
+    console.log("true")
+} else {
+    console.log("false");
+}
