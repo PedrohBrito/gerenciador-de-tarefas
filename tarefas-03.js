d5cd0e9eca24d04dd2fb06f818e42278c18b5b51
Pedro <pedro.h.cunha16@aluno.senai.br>
2025-05-30 09:23:08 -0300

    feat: adiciona tarefa no ínicio

---
+++ tarefas-03.js
@@ -0,0 +1,8 @@
+let tarefas = ["Estudar JavaScript", "Fazer exercícios de matemática", "Lavar a louça", 
+    "Comprar mercado", "Ir à academia", "Ler um livro", "Fazer revisão para o teste", 
+    "Limpar o quarto", "Estudar para concurso", "Organizar o computador"];
+
+// Adicionando uma tarefa no início da lista
+tarefas.unshift("Lavar o banheiro");
+console.log(tarefas);
+
