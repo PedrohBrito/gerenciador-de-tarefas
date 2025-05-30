4f8093063c979e779a2927663842beac3e1376d6
Pedro <pedro.h.cunha16@aluno.senai.br>
2025-05-30 09:14:09 -0300

    Alteração no arquivo tarefas-01.js

--- tarefas-02.js
+++ tarefas-02.js
@@ -2,8 +2,5 @@ let tarefas = ["Estudar JavaScript", "Fazer exercícios de matemática", "Lavar 
     "Comprar mercado", "Ir à academia", "Ler um livro", "Fazer revisão para o teste", 
     "Limpar o quarto", "Estudar para concurso", "Organizar o computador"];
 
-// Adicionando 2 novas tarefas
- tarefas.push("Lavar as roupas", "Estender as roupas");
-console.log(tarefas);
-
- 
+// Removendo a última tarefa 
+tarefas.pop();
