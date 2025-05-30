88e5530f27d7b2a809d5ea7107e5a78a219e860b
Pedro <pedro.h.cunha16@aluno.senai.br>
2025-05-30 11:03:35 -0300

    feat: Concatena listas de tarefas

---
+++ tarefas-09.js
@@ -0,0 +1,7 @@
+let tarefas = ["Estudar JavaScript", "Fazer exercícios de matemática", "Lavar a louça", 
+    "Comprar mercado", "Ir à academia", "Ler um livro", "Fazer revisão para o teste", 
+    "Limpar o quarto", "Estudar para concurso", "Organizar o computador"];
+
+let array2Tarefas = ["Jogar", "Sair pro shopping"];
+ // Concatena listas de tarefas
+console.log(`Tarefas combinadas : ${tarefas.concat(array2Tarefas)}`);
